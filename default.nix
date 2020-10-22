2fba0c2052e8d2300fd03f323ca04830888a7c3a
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-10-22 11:46:36 -0700

    add python3 for make check (not running?)

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ stdenv, autoreconfHook }:
+{ stdenv, autoreconfHook, python3 }:
 
 stdenv.mkDerivation rec {
   name = "easel-${version}";
   version = "0.47";
   src = ./.;
   nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = [ python3 ];
 #   builder = builtins.writeScript "builder.sh" ''
 #     autoconf
 #     ./configure
