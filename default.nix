47daa7c1020224be9312c4f5cd6953462f659306
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-10-22 13:43:24 -0700

    patch shabangs to fix perl

--- default.nix
+++ default.nix
@@ -1,10 +1,13 @@
-{ stdenv, autoreconfHook, python3 }:
+{ stdenv, autoreconfHook, python3, perl }:
 
 stdenv.mkDerivation rec {
   name = "easel-${version}";
   version = "0.47";
   src = ./.;
   nativeBuildInputs = [ autoreconfHook ];
-  buildInputs = [ python3 ];
+  buildInputs = [ python3 perl ];
   doCheck = true;
+  postPatch = ''
+    patchShebangs --build *
+  '';
 }
